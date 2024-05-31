cb93f0d39488414c4b567ac6e733836a5589b9ff
ArashNabaei <Arash_ANB@yahoo.com>
2024-05-31 09:47:43 -0700

    Add ProductionUnit file

---
+++ OrderBalancer/OrderBalancer/Entities/ProductionUnit.cs
@@ -0,0 +1,20 @@
+﻿
+namespace OrderBalancer.Entities
+{
+    public class ProductionUnit
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Capacity { get; set; }
+
+        public bool IsBusy { get; set; }
+
+        private Order? CurrentOrder { get; set; }
+
+    }
+}
