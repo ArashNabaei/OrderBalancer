fc33586009bb92b5baf3d40e08d32d579d9bd529
ArashNabaei <Arash_ANB@yahoo.com>
2024-05-31 09:36:05 -0700

    Add Order class

---
+++ OrderBalancer/OrderBalancer/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿
+namespace OrderBalancer.Entities
+{
+    public class Order
+    {
+
+        public string Desctiption { get; set; }
+
+        public List<Food> Foods { get; set; }
+
+        public int ProcessingTime { get; set; }
+
+    }
+}
